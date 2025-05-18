05d35c4051d6cbebebd9bc49674a9a10ff379b1b
Station Chenqiao <i@cctv3.net>
2025-05-18 20:43:41 +0800

    fit: 钱包，添加列表展示 ...

--- src/views/wallet/index.vue
+++ src/views/wallet/index.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import ApexCharts from 'apexcharts'
 import { useRoute } from 'vue-router'
 import { NextService } from '@/service'
@@ -7,6 +7,8 @@ import moment from 'moment'
 
 const chartRef = ref<HTMLElement | null>(null)
 const chart = ref<ApexCharts | null>(null)
+const datas = ref<any[]>([])
+
 const options = ref({
   chart: {
     type: 'line',
@@ -38,6 +40,7 @@ const loadProperty = async () => {
     currentPage: 1,
     pageSize: 52,
   })
+  datas.value = [...result.data.records]
   const records = result.data.records.reverse()
   const newData = records.map((item: any) => (item.total / 10).toFixed(2))
   const newCategories = records.map((item: any) => moment(item.settleDate).format('YY年ww周'))
@@ -65,19 +68,80 @@ onMounted(() => {
   }
   loadProperty()
 })
+
+const maps = {
+  wechat: '微信',
+  alipay: '支付宝',
+  unionpay: '银联',
+  cash: '现金',
+  carpooling: '顺风车',
+  eastmoney: '股票',
+  housefund: '公积金',
+  usd: '美元',
+}
 </script>
 
 <template>
   <div class="wallet">
-    <h1>Wallet</h1>
-    <p>Welcome to the wallet page!</p>
-    <van-button type="primary" @click="loadProperty">刷新图表</van-button>
-    <div ref="chartRef" style="width: 100%; height: 300px"></div>
+    <div ref="chartRef" style="width: 100vw" />
+    <div v-for="(item, index) in datas" :key="index" class="wallet-item">
+      <div class="wallet-top">
+        <span class="wallet-date">{{ moment(item.settleDate).format('YYYY年ww周') }}</span>
+        <span class="wallet-total" :style="{ color: '#333' }">
+          {{ parseFloat(item.total).toFixed(2) }}K
+        </span>
+      </div>
+      <div style="height: 6px" />
+      <div class="wallet-details">
+        <div class="wallet-detail" v-for="(label, key) in maps" :key="key">
+          <span class="label">{{ label }}: </span>
+          <span class="value">
+            {{
+              Array.isArray(item[key])
+                ? `[${item[key].map((t) => (key === 'usd' ? '$' : '') + t + 'K').join(' & ')}]`
+                : item[key] + 'K'
+            }}
+          </span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .wallet {
-  padding: 16px;
+  .wallet-item {
+    padding: 6px 16px;
+    border-bottom: 1px solid #eee;
+    cursor: pointer;
+  }
+  .wallet-top {
+    display: flex;
+    justify-content: space-between;
+  }
+  .wallet-date {
+    font-size: 16px;
+    color: #333;
+  }
+  .wallet-total {
+    font-size: 16px;
+  }
+  .wallet-details {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 6px;
+  }
+  .wallet-detail {
+    display: flex;
+  }
+  .label {
+    font-size: 14px;
+    color: #333;
+    margin-right: 4px;
+  }
+  .value {
+    font-size: 14px;
+    color: #999;
+  }
 }
 </style>
