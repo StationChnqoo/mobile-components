2f36d38984b64b9c4649b827ddd6616f517fa7b1
Station Chenqiao <i@cctv3.net>
2025-05-18 12:12:43 +0800

    chore: 配置vant@4 ...

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+// 1. 引入你需要的组件
+import { Button } from 'vant'
+// 2. 引入组件样式
+import 'vant/lib/index.css'
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +13,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+let vantComponents = [Button]
+for (let vc of vantComponents) {
+  app.use(vc)
+}
 app.mount('#app')
---
+++ src/views/wallet/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="wallet">
+    <h1>Wallet</h1>
+    <p>Welcome to the wallet page!</p>
+    <van-button type="primary">主要按钮</van-button>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
